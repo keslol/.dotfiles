7bb943dc7adfc02dc8f0f6f63d6a18da6d0ee124
keslol <git@kes.lol>
2024-05-23 13:13:24 +0200

    nvim config = lazyvim

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.swapfile = false
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.python3_host_prog = "/home/keslol/.local/share/mise/installs/python/3.12.3/bin/python"
+vim.g.vimtex_view_method = "zathura"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
