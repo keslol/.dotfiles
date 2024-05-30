64175eef9163fae9bc43d662f2ce42c705e47024
keslol <git@kes.lol>
2024-05-30 20:07:18 +0200

    updated lazyvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,6 +4,7 @@
 vim.opt.swapfile = false
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+vim.g.lazyvim_python_ruff = "ruff"
 vim.g.python3_host_prog = "/home/keslol/.local/share/mise/installs/python/3.12.3/bin/python"
 vim.g.vimtex_view_method = "zathura"
 vim.opt.tabstop = 4
